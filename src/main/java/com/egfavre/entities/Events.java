65bda824602b7ed7456a006cfd6fcc9fae440c78
Liz Favre <egfavre@gmail.com>
2016-06-23 11:30:55 -0400

    add classes

---
+++ src/main/java/com/egfavre/entities/Events.java
@@ -0,0 +1,66 @@
+package com.egfavre.entities;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Created by user on 6/23/16.
+ */
+@Entity
+@Table(name = "events")
+public class Events {
+
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
+    String description;
+
+    @Column(nullable = false)
+    LocalDateTime time;
+
+    @ManyToOne
+    User user;
+
+    public Events() {
+    }
+
+    public Events(String description, LocalDateTime time, User user) {
+        this.description = description;
+        this.time = time;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
